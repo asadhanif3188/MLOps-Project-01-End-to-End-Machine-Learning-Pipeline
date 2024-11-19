7ee108cdf0e1d4489f8756b62629ff3c7d2d35a9
Asad Hanif <asadhanif3188@gmail.com>
2024-11-19 13:51:26 +0500

    preprocess.py file updated

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,6 +3,29 @@ import sys
 import yaml
 import os
 
-# Load parameters from param.yaml
-params = yaml.safe_load(open("params.yaml"))
 
+def preprocess(input_path, output_path):
+    """
+    Preprocess the data by reading the input file, 
+    dropping the 'Unnamed: 0' column,
+    and saving the preprocessed data to a CSV file.
+    """
+    # Read the input file
+    df = pandas.read_csv(input_path)
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    
+    # Save the preprocessed data to a CSV file
+    df.to_csv(output_path, header=None, index=False)
+
+    print(f'Preprocessed data saved to {output_path}')
+
+if __name__ == "__main__":
+    # Load parameters from param.yaml
+    params = yaml.safe_load(open("params.yaml"))['preprocess']
+
+    # Call the preprocess function with the input and output paths
+    preprocess(
+        params['input'], 
+        params['output']
+        )
